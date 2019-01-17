ae36142c35c835d32f34d0a636160efdc6b0172a
MarekBeers <44866515+MarekBeers@users.noreply.github.com>
2019-01-17 16:13:54 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-cd /home/pi/spigot/
-/usr/bin/screen -dmS minecraft /usr/lib/jvm/java-9-openjdk-armhf/bin/java -jar -Xms400M -Xmx800M /home/pi/spigot/spigot.jar
+cd /home/pi/minecraft/
+/usr/bin/screen -dmS minecraft /usr/lib/jvm/java-9-openjdk-armhf/bin/java -jar -Xms800M -Xmx900M /home/pi/spigot/spigot.jar
 screen -r minecraft
